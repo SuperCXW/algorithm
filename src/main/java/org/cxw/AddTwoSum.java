372810367b6569ccbc8a7fe0ea5b6ff3258609bc
司诚 <chengxinwei.cxw@alibaba-inc.com>
2023-09-07 16:26:26 +0800

    init

---
+++ src/main/java/org/cxw/AddTwoSum.java
@@ -0,0 +1,66 @@
+package org.cxw;
+
+/**
+ * @author 司诚
+ * @description
+ * @date 2023/9/7
+ */
+public class AddTwoSum {
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    //两数之和，主要是看链表双指针
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode left = l1, right = l2;
+
+        ListNode dumy = new ListNode(-1);
+        ListNode tmp = dumy;
+
+        int carry = 0;
+
+        while (left != null || right != null || carry > 0) {
+            int val = carry;
+            if (left != null) {
+                val += left.val;
+                left = left.next;
+            }
+
+            if (right != null) {
+                val += right.val;
+                right = right.next;
+            }
+
+            int num = val / 10;
+            carry = val % 10;
+
+            tmp.next = new ListNode(num);
+            tmp = tmp.next;
+        }
+        return dumy.next;
+    }
+}
---
+++ src/main/java/org/cxw/App.java
@@ -0,0 +1,13 @@
+package org.cxw;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
